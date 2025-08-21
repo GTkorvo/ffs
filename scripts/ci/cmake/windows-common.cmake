9f4bc56092c56a7f817926373d3bebcbd46ba889
Greg Eisenhauer <eisen@cc.gatech.edu>
2025-08-21 09:38:21 -0400

    wip

--- scripts/ci/cmake/windows-common.cmake
+++ scripts/ci/cmake/windows-common.cmake
@@ -6,7 +6,7 @@ string(APPEND dashboard_cache "
 list(APPEND CTEST_UPDATE_NOTES_FILES "${CMAKE_CURRENT_LIST_FILE}")
 
 # the two lines below shouldn't be necessary, but something wrong with vcpkg maybe
-set(ENV{dill_DIR} "${CMAKE_CURRENT_LIST_DIR}/../../../../dill/install/lib/cmake/dill")
-set(ENV{atl_DIR} "${CMAKE_CURRENT_LIST_DIR}/../../../../atl/install/lib/cmake/atl")
+set(ENV{dill_DIR} "${CMAKE_CURRENT_LIST_DIR}/../../../../lib/Release/cmake/dill")
+set(ENV{atl_DIR} "${CMAKE_CURRENT_LIST_DIR}/../../../../lib/Releasecmake/atl")
 
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
