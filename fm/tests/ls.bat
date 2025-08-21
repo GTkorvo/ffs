2d227f796c42ee835149c3073f9ac7b0974abfc6
Greg Eisenhauer <eisen@cc.gatech.edu>
2025-08-21 07:45:21 -0400

    wip

--- fm/tests/ls.bat
+++ fm/tests/ls.bat
@@ -1,1 +1,15 @@
+echo ffs.dll
+where ffs.dll
+echo
+echo api-ms-win-crt-heap-l1-1-0.dll
+where api-ms-win-crt-heap-l1-1-0.dll
+echo
+echo VCRUNTIME140.dll
+where VCRUNTIME140.dll
+echo
+echo KERNEL32.dll
+where KERNEL32.dll
+echo
+echo dir
+echo
 dir /s /b /o:gn D:\a\ffs\ffs\
