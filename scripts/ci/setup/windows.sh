4da2f10727a7c9410d1af20f01bed66c49298788
Greg Eisenhauer <eisen@cc.gatech.edu>
2025-09-04 10:09:24 -0400

    wip

--- scripts/ci/setup/windows.sh
+++ scripts/ci/setup/windows.sh
@@ -8,11 +8,3 @@ echo "checking key"
 reg query "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\" "//v" "SafeDllSearchMode" || true
 echo "directory"
 reg query "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager"
-echo $PATH
-echo "PATH = ${PATH}"
-echo "pwd = ${PWD}"
-export PATH=${PATH}:${PWD}/atl/install/Release/lib:${PWD}/atl/install/Release/bin:${PWD}/dill/install/Release/lib:${PWD}/dill/install/Release/bin:${PWD}/dill/install/lib:${PWD}/dill/install/bin:${PWD}/atl/install/lib:${PWD}/atl/install/bin:
-echo "newpath = ${PATH}"
-echo 'export PATH=Spuriousdir:${PATH}:${PWD}/atl/install/Release/lib:${PWD}/atl/install/Release/bin:${PWD}/dill/install/Release/lib:${PWD}/dill/install/Release/bin:' >> ~/.bashrc
-cat ~/.bashrc
-chmod ugo+rx ~/.bashrc
